234bd5e2a6b9296540b1775b18c93553640ae1d3
mes3n <skidmackan@gmail.com>
2022-10-06 23:56:57 +0200

    added toggle for turning script on and off

--- main.py
+++ main.py
@@ -3,8 +3,36 @@ from flask import Flask, request, render_template, redirect, url_for, send_file
 from pathlib import Path
 import json
 
+import threading
+import asyncio
+
+import subprocess
+
 
 app = Flask(__name__)
+
+
+class ScriptThread():
+    def __init__(self, script_directory):
+        self.process: subprocess.Popen = None
+        self.script_dir: str = script_directory
+        self.pid = '0'
+
+        self.running = False
+
+    def start(self):
+        commands = f'cd {self.script_dir}; python main.py'
+        self.process = subprocess.Popen(commands, stdout=subprocess.PIPE, shell=True)
+        self.pid = self.process.pid
+
+        self.running = True
+
+    def stop(self):
+        commands = f'kill {self.pid}'
+        self.process = subprocess.Popen(commands, stdout=subprocess.PIPE, shell=True)
+        
+        self.running = False
+
 
 
 @app.route('/')
@@ -71,7 +99,7 @@ def set_parameter():
                 global_parameters[i] = new_parameter
 
                 if global_parameters:
-                    with open(parameters_path, 'w') as file:
+                    with open(script_parameters, 'w') as file:
                         file.write(json.dumps(global_parameters, indent=4))
 
                 return redirect(url_for('home', status=f"Updated parameter: {new_parameter['name']}."))
@@ -80,7 +108,7 @@ def set_parameter():
             global_parameters.append(new_parameter)
 
             if global_parameters:
-                with open(parameters_path, 'w') as file:
+                with open(script_parameters, 'w') as file:
                     file.write(json.dumps(global_parameters, indent=4))
 
             return redirect(url_for('home', status=f"Added parameter: {new_parameter['name']}."))
@@ -89,13 +117,14 @@ def set_parameter():
         return redirect(url_for('home', status=f"Error: method not found."))
 
 
-
 @app.route('/settings')
 def settings ():
-    return render_template('settings.html', config=global_config)  # shouldn't be duplicates 
+    return render_template('settings.html', config=global_config, script_should_run=script_should_run)  # shouldn't be duplicates 
+
 
 @app.route('/set_settings', methods=['GET', 'POST'])
 def set_settings ():
+    global script_should_run
 
     if request.form['action'] == 'cancel':
         return redirect(url_for('home'))
@@ -103,12 +132,19 @@ def set_settings ():
     elif request.form['action'] == 'save':
 
         global_config['TOKEN'] = request.form.get('tibber_token', type=str)
+        global_config['husdata_ip'] = request.form.get('husdata_ip', type=str)
         global_config['low_mid'] = request.form.get('low_mid_price', type=float)
         global_config['mid_high'] = request.form.get('mid_high_price', type=float)
+        script_should_run = request.form.get('script_toggle_on') != None
 
         if global_config:
-            with open(varmescript_config, 'w') as file:
+            with open(script_config, 'w') as file:
                 file.write(json.dumps(global_config, indent=4))
+
+        if script_should_run and not script_thread.running:
+            script_thread.start()
+        elif not script_should_run and script_thread.running:
+            script_thread.stop()
 
         return redirect(url_for('home', status=f"Saved settings."))
 
@@ -118,33 +154,44 @@ def set_settings ():
 
 @app.route('/database')
 def database():
-    return send_file(database_path)
+    return send_file(script_database)
 
+
+def start_script():
+    subprocess.run([f'cd {script_dir};', 'python main.py &'], shell=True)
+    pid = int(subprocess.run(['echo $!'], capture_output=True, shell=True))
 
 if __name__ == '__main__':
 
-    varmescript_path = Path('../varmescript/')
-    parameters_path = Path(varmescript_path.joinpath('config/parameters.json'))
-    
-    varmescript_config = Path(varmescript_path.joinpath('config/config.json'))
+    paths = Path('paths.json')
+    if paths.is_file():
+        with open(paths, 'r') as file:
+            paths_dict = json.load(file)
+            script_dir = Path(paths_dict['script_dir'])    
+            script_config = Path(paths_dict['script_config'])
+            script_database = Path(paths_dict['script_database'])
+            script_parameters = Path(paths_dict['script_parameters'])    
+    else:
+        script_dir = Path('')    
+        script_config = Path('')
+        script_database = Path('')
+        script_parameters = Path('')
 
-    database_path = Path(varmescript_path.joinpath('database/husdata.csv'))
-
-    if parameters_path.is_file():
-        with open(parameters_path, 'r') as file:
+    if script_parameters.is_file():
+        with open(script_parameters, 'r') as file:
             global_parameters = json.load(file)
-
     else:
         global_parameters = []
 
-    if varmescript_config.is_file():
-        with open(varmescript_config, 'r') as file:
+    if script_config.is_file():
+        with open(script_config, 'r') as file:
             global_config = json.load(file)
-
     else:
         global_config = {}
 
+    script_should_run: bool = False
+    script_thread: ScriptThread = ScriptThread(script_dir)
 
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=5000, debug=True)
     # from waitress import serve
     # serve(app, host="0.0.0.0", port=5000)
